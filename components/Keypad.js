b6843a3452381f5b9316b34feab23220a0d7f07e
Alex <reybislp@gmail.com>
2021-08-11 10:17:11 -0400

    refactoring fixing grid

--- components/Keypad.js
+++ components/Keypad.js
@@ -4,98 +4,82 @@ const Keypad = () => {
   const buttons = [
     {
       text: "7",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "8",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "9",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "del",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyBack border-skin-keyBackShadow pt-1",
-      buttonClass: "text-lg text-white uppercase",
+      containerClass: "bg-skin-keyBack border-skin-keyBackShadow pt-1",
+      buttonClass: "text-sm sm:text-lg text-white uppercase",
     },
     {
       text: "4",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "5",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "6",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "+",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor",
     },
     {
       text: "1",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "2",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "3",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "-",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor",
     },
     {
       text: ".",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor",
     },
     {
       text: "0",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
     },
     {
       text: "/",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor",
     },
     {
       text: "x",
-      containerClass:
-        "w-12 lg:w-20 h-10 lg:h-12 bg-skin-keyColor border-skin-KeyShadow",
+      containerClass: "bg-skin-keyColor border-skin-KeyShadow",
       buttonClass: "text-2xl text-skin-keytextColor",
     },
     {
@@ -117,7 +101,7 @@ const Keypad = () => {
 
   return (
     <div className="bg-skin-toggleBackground transition duration-500 ease-in-out h-auto mt-5 rounded-md shadow w-auto">
-      <div className="p-4 grid grid-cols-4 gap-3 ">{renderButtons}</div>
+      <div className="p-4 grid grid-cols-4 gap-5">{renderButtons}</div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
-import Head from 'next/head'
-import Calculator from './../components/Calculator';
+import Head from "next/head";
+import Calculator from "./../components/Calculator";
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="Calculator" content="Challenge by Frontend Mentor. Coded by https://github.com/alopezprlp." />
+        <title>Calculator Challenge</title>
+        <meta
+          name="Calculator"
+          content="Challenge by Frontend Mentor. Coded by https://github.com/alopezprlp."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <Calculator/>
+      <Calculator />
     </>
-  )
+  );
 }

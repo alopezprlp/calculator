02809281606e4466ba487577142a078f73a7bb0b
Alex <reybislp@gmail.com>
2021-08-11 10:28:21 -0400

    change rounded button corners

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ text, containerClass, buttonClass }) => (
   <div
-    className={`${containerClass} transition duration-500 ease-in-out text-center lg:pt-2 rounded shadow border-b-4 cursor-pointer`}
+    className={`${containerClass} transition duration-500 ease-in-out text-center lg:pt-2 rounded-md shadow border-b-4 cursor-pointer`}
   >
     <button className={` ${buttonClass} font-bold font-mono`}>{text}</button>
   </div>

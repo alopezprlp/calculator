4e11123054c7addd60b6cb18728712acabbb3894
Alex <reybislp@gmail.com>
2021-08-11 11:15:04 -0400

    change styles

--- components/Button.js
+++ components/Button.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Button = ({ text, containerClass, buttonClass }) => (
   <div
-    className={`${containerClass} transition duration-500 ease-in-out text-center lg:pt-2 rounded-md shadow border-b-4 cursor-pointer`}
+    className={`${containerClass} transition duration-500 ease-in-out text-center rounded-md shadow border-b-4 cursor-pointer pt-2`}
   >
-    <button className={` ${buttonClass} font-spartan`}>{text}</button>
+    <button className={` ${buttonClass} font-spartan `}>{text}</button>
   </div>
 );
 
--- components/Keypad.js
+++ components/Keypad.js
@@ -5,93 +5,93 @@ const Keypad = () => {
     {
       text: "7",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "8",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "9",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "del",
-      containerClass: "bg-skin-keyBack border-skin-keyBackShadow pt-1",
+      containerClass: "bg-skin-keyBack border-skin-keyBackShadow pt-4",
       buttonClass: "text-sm sm:text-lg text-white uppercase",
     },
     {
       text: "4",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "5",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "6",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "+",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "1",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "2",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "3",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "-",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: ".",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "0",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor text-btnFont",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "/",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "x",
       containerClass: "bg-skin-keyColor border-skin-KeyShadow",
-      buttonClass: "text-2xl text-skin-keytextColor",
+      buttonClass: "text-skin-keytextColor text-btnFont",
     },
     {
       text: "reset",
       containerClass: "col-span-2 bg-skin-keyBack border-skin-keyBackShadow",
-      buttonClass: "text-2xl text-white uppercase",
+      buttonClass: "text-white uppercase",
     },
     {
       text: "=",
       containerClass:
         "col-span-2 bg-skin-keyRedColor border-skin-keyRedColorShadow",
-      buttonClass: "text-2xl text-skin-equalText",
+      buttonClass: "text-skin-equalText",
     },
   ];
 
@@ -101,7 +101,7 @@ const Keypad = () => {
 
   return (
     <div className="bg-skin-toggleBackground transition duration-500 ease-in-out h-auto mt-5 rounded-md shadow w-auto">
-      <div className="p-4 grid grid-cols-4 gap-5">{renderButtons}</div>
+      <div className="p-4 grid grid-cols-4 gap-4">{renderButtons}</div>
     </div>
   );
 };
